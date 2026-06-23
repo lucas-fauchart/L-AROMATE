88624e2887b0ccbbc8e568fa752a60f657f690ca
Lucas <lucasfauchart75@gmail.com>
2026-06-23 23:58:42 +0200

    <feat>: Création barre de navigation mobile
    Création de la barre de navigation mobile avec le burger à droite et le logo gauche

--- l-aromate/src/app/page.tsx
+++ l-aromate/src/app/page.tsx
@@ -4,7 +4,11 @@ import BarreNavigation from "./composants/barreNavigation";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <BarreNavigation/>
+      <BarreNavigation 
+        categoriesGauche={["Accueil", "La Carte", "Notre Histoire"]} 
+        categoriesDroite={["Les Aromes", "Réservation", "Contact"]} 
+        tailleTexte="text-lg"
+      />
     </div>
   );
 }
