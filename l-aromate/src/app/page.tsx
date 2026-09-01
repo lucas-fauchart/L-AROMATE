7e6ce7763da96cd861b78f69977b575b76a97b63
Lucas <lucasfauchart75@gmail.com>
2026-09-01 10:09:21 +0200

    <feat>: Texte et bouton de l'en tête
    Ajout du texte de présentation rapide et d'un bouton pour la réservation d'une table

--- l-aromate/src/app/page.tsx
+++ l-aromate/src/app/page.tsx
@@ -15,6 +15,10 @@ export default function Home() {
       <main>
         <Section className="mb-30 flex flex-col items-center text-center">
           <ImageElement src="/nom_blanc_aromate.png" alt="Nom du restaurant L'Aromate" className="w-38 xs:w-9 sm:w-10 animation-apparition-simple-fondu" width={754} height={195}/>
+          <p className="pt-8 pb-10 px-6">Une exploration des aromates essentiels de la cuisine, 
+            ou chaque herbe dévoile ses arôme uniques pour enrichir et sublimer l’expérience culinaire
+          </p>
+          <button className="bg-[#FED17C] py-2 px-6 rounded-full text-[#21233C] mb-12">Réserver une table</button>
         </Section>
       </main>
     
